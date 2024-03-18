d79ad7a4ca538e1e21f61522fdbd1f676b97b53b
sohaib <sohaib.sharih@gmail.com>
2024-03-18 09:29:28 +0500

    first

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,77 @@
+// // The below category import is not there in the video.
+// import { Category } from '@mui/icons-material';
+// import {Stack} from '@mui/material';
+// import { categories } from '../utils/constants';
+
+// // const selectedCategory = 'New';
+// const Sidebar = ({selectedCategory, setSelectedCategory}) => (
+
+// <Stack direction="row"
+// sx={{
+//     overflow: "auto",
+//     height: { sx: 'auto', md: '95%'},
+//     flexDirection: {md: 'column'},
+// }}
+// >
+// {categories.map((category) => (
+// <button
+//     className = "category-btn"
+//     onClick={() => setSelectedCategory(category.name)}
+
+//     style={{
+//         background: category.name === selectedCategory && '#FC1503',
+//         color: 'white'
+//     }}
+//     key={category.name}
+    
+//     >
+//     <span style={{color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px'}}>{category.icon}</span>
+//     <span style={{ opactity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
+
+// </button>
+
+// ))}
+
+// </Stack>
+
+// )
+  
+
+// export default Sidebar
+
+import React from "react";
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+
+const Categories = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
+          {category.icon}
+        </span>
+        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Categories;
